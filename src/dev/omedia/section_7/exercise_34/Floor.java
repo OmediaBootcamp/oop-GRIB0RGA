419087207236811ec6a7adb51c4eefb1d7e578ef
Griborga <giorgi4@discovery.co.za>
2024-10-10 12:03:57 +0400

    [Section - 7][Exercise - 34] - Calculator

---
+++ src/dev/omedia/section_7/exercise_34/Floor.java
@@ -0,0 +1,15 @@
+package dev.omedia.section_7.exercise_34;
+
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+}
