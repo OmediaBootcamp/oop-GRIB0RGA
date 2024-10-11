ed4bd8eff9e988cc858569067ff2fcf1fa64e47f
Griborga <giorgi4@discovery.co.za>
2024-10-11 16:57:43 +0400

    [Section - 7][Exercise - 38] - Composition

---
+++ src/dev/omedia/section_8/exercise_38/Wall.java
@@ -0,0 +1,14 @@
+package dev.omedia.section_8.exercise_38;
+
+public class Wall {
+
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
